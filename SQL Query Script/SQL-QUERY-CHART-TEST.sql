44939bf63be95b0048ad0759a137e257b78c04e1
Danial Haris <39772417+dannyalharris@users.noreply.github.com>
2021-06-05 18:30:24 +0200

    Update SQL-QUERY-CHART-TEST.sql

--- SQL Query Script/SQL-QUERY-CHART-TEST.sql
+++ SQL Query Script/SQL-QUERY-CHART-TEST.sql
@@ -2,10 +2,10 @@
 GO
 DECLARE @Counter INT 
 SET @Counter=1
-WHILE
+WHILE ( @Counter <= 10000)
 BEGIN
     INSERT INTO Patient(Timestamp, O2Level) Values ('20100301', @Counter)
     PRINT 'The counter value is = ' + CONVERT(VARCHAR,@Counter)
     SET @Counter  = @Counter  + 1
-   -- WAITFOR DELAY '00:00:01';
+    WAITFOR DELAY '00:00:01';
 END
