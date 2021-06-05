bd8080e27c07c050c50cb65e0501c9a6e8e46594
Kai Li Ong <kailiong.kylie@gmail.com>
2021-06-05 15:19:00 +0200

    FusionCharts

--- Pages/PatientsData/Index.cshtml.cs
+++ Pages/PatientsData/Index.cshtml.cs
@@ -10,107 +10,66 @@ using Newtonsoft.Json;
 using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.Logging;
 using System.Runtime.Serialization;
-//using System.Web.Mvc;
+using System.Data;
+using FusionCharts.DataEngine;
+using FusionCharts.Visualization;
+
 
 
 namespace Corona_Ventilator.Pages.PatientsData
 {
-	//[DataContract]
-	//public class DataPoint
-	//{
-	//	public DataPoint(double x, double y)
-	//	{
-	//		this.X = x;
-	//		this.Y = y;
-	//	}
-
-	//	//Explicitly setting the name to be used while serializing to JSON.
-	//	[DataMember(Name = "x")]
-	//	public Nullable<double> X = null;
-
-	//	//Explicitly setting the name to be used while serializing to JSON.
-	//	[DataMember(Name = "y")]
-	//	public Nullable<double> Y = null;
-	//}
 
 	public class IndexModel : PageModel
     {
-       
+        // create a public property. OnGet method() set the chart configuration json in this property.
+        // When the page is being loaded, OnGet method will be  invoked
+        public string ChartJson { get; internal set; }
         public void OnGet()
         {
+            // create data table to store data
+            DataTable ChartData = new DataTable();
+            // Add columns to data table
+            ChartData.Columns.Add("Programming Language", typeof(System.String));
+            ChartData.Columns.Add("Users", typeof(System.Double));
+            // Add rows to data table
 
+            ChartData.Rows.Add("Java", 62000);
+            ChartData.Rows.Add("Python", 46000);
+            ChartData.Rows.Add("Javascript", 38000);
+            ChartData.Rows.Add("C++", 31000);
+            ChartData.Rows.Add("C#", 27000);
+            ChartData.Rows.Add("PHP", 14000);
+            ChartData.Rows.Add("Perl", 14000);
+
+            // Create static source with this data table
+            StaticSource source = new StaticSource(ChartData);
+            // Create instance of DataModel class
+            DataModel model = new DataModel();
+            // Add DataSource to the DataModel
+            model.DataSources.Add(source);
+            // Instantiate Column Chart
+            Charts.ColumnChart column = new Charts.ColumnChart("first_chart");
+            // Set Chart's width and height
+            column.Width.Pixel(700);
+            column.Height.Pixel(400);
+            // Set DataModel instance as the data source of the chart
+            column.Data.Source = model;
+            // Set Chart Title
+            column.Caption.Text = "Most popular programming language";
+            // Set chart sub title
+            column.SubCaption.Text = "2017-2018";
+            // hide chart Legend
+            column.Legend.Show = false;
+            // set XAxis Text
+            column.XAxis.Text = "Programming Language";
+            // Set YAxis title
+            column.YAxis.Text = "User";
+            // set chart theme
+            column.ThemeName = FusionChartsTheme.ThemeName.FUSION;
+            // set chart rendering json
+            ChartJson = column.Render();
         }
-        //GET: Home
-        //public ActionResult Index()
-        //{
-        //    return ;
-        //}
-
-  //      public ContentResult JSON()
-		//{
-		//	List<DataPoint> dataPoints = new List<DataPoint>();
-
-
-		//	dataPoints.Add(new DataPoint(1481999400000, 4.67));
-		//	dataPoints.Add(new DataPoint(1482604200000, 4.7));
-		//	dataPoints.Add(new DataPoint(1483209000000, 4.96));
-		//	dataPoints.Add(new DataPoint(1483813800000, 5.12));
-		//	dataPoints.Add(new DataPoint(1484418600000, 5.08));
-		//	dataPoints.Add(new DataPoint(1485023400000, 5.11));
-		//	dataPoints.Add(new DataPoint(1485628200000, 5));
-		//	dataPoints.Add(new DataPoint(1486233000000, 5.2));
-		//	dataPoints.Add(new DataPoint(1486837800000, 4.7));
-		//	dataPoints.Add(new DataPoint(1487442600000, 4.74));
-		//	dataPoints.Add(new DataPoint(1488047400000, 4.67));
-		//	dataPoints.Add(new DataPoint(1488652200000, 4.66));
-		//	dataPoints.Add(new DataPoint(1489257000000, 4.86));
-		//	dataPoints.Add(new DataPoint(1489861800000, 4.91));
-		//	dataPoints.Add(new DataPoint(1490466600000, 5.12));
-		//	dataPoints.Add(new DataPoint(1491071400000, 5.4));
-		//	dataPoints.Add(new DataPoint(1491676200000, 5.08));
-		//	dataPoints.Add(new DataPoint(1492281000000, 5.05));
-		//	dataPoints.Add(new DataPoint(1492885800000, 4.98));
-		//	dataPoints.Add(new DataPoint(1493490600000, 4.89));
-		//	dataPoints.Add(new DataPoint(1494095400000, 4.9));
-		//	dataPoints.Add(new DataPoint(1494700200000, 4.95));
-		//	dataPoints.Add(new DataPoint(1495305000000, 4.88));
-		//	dataPoints.Add(new DataPoint(1495909800000, 5.07));
-		//	dataPoints.Add(new DataPoint(1496514600000, 5.14));
-		//	dataPoints.Add(new DataPoint(1497119400000, 5.05));
-		//	dataPoints.Add(new DataPoint(1497724200000, 5.03));
-		//	dataPoints.Add(new DataPoint(1498329000000, 4.93));
-		//	dataPoints.Add(new DataPoint(1498933800000, 4.97));
-		//	dataPoints.Add(new DataPoint(1499538600000, 4.86));
-		//	dataPoints.Add(new DataPoint(1500143400000, 4.95));
-		//	dataPoints.Add(new DataPoint(1500748200000, 4.83));
-		//	dataPoints.Add(new DataPoint(1501353000000, 4.83));
-		//	dataPoints.Add(new DataPoint(1501957800000, 4.73));
-		//	dataPoints.Add(new DataPoint(1502562600000, 4.56));
-		//	dataPoints.Add(new DataPoint(1503167400000, 4.34));
-		//	dataPoints.Add(new DataPoint(1503772200000, 4.25));
-		//	dataPoints.Add(new DataPoint(1504377000000, 4.18));
-		//	dataPoints.Add(new DataPoint(1504981800000, 4.22));
-		//	dataPoints.Add(new DataPoint(1505586600000, 4.18));
-		//	dataPoints.Add(new DataPoint(1506191400000, 4.31));
-		//	dataPoints.Add(new DataPoint(1506796200000, 4.34));
-		//	dataPoints.Add(new DataPoint(1507401000000, 4.47));
-		//	dataPoints.Add(new DataPoint(1508005800000, 4.57));
-		//	dataPoints.Add(new DataPoint(1508610600000, 4.63));
-		//	dataPoints.Add(new DataPoint(1509215400000, 4.55));
-		//	dataPoints.Add(new DataPoint(1509820200000, 4.55));
-		//	dataPoints.Add(new DataPoint(1510425000000, 4.44));
-		//	dataPoints.Add(new DataPoint(1511029800000, 4.46));
-		//	dataPoints.Add(new DataPoint(1511634600000, 4.41));
-		//	dataPoints.Add(new DataPoint(1512239400000, 4.3));
-		//	dataPoints.Add(new DataPoint(1512844200000, 4.31));
-		//	dataPoints.Add(new DataPoint(1513449000000, 4.3));
-		//	dataPoints.Add(new DataPoint(1513621800000, 4.36));
-
-
-		//	JsonSerializerSettings _jsonSetting = new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore };
-		//	return Content(JsonConvert.SerializeObject(dataPoints, _jsonSetting), "application/json");
-		//}
-
+ 
 	}
 }
 
