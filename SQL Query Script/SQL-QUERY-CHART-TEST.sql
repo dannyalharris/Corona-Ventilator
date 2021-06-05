42c7d9a1b72f43734590b8cc2a3d7700e159203c
Danial Haris <39772417+dannyalharris@users.noreply.github.com>
2021-06-05 18:10:49 +0200

    SQL-QUERY

---
+++ SQL Query Script/SQL-QUERY-CHART-TEST.sql
@@ -0,0 +1,11 @@
+﻿USE [Corona-Ventilator-Database]
+GO
+DECLARE @Counter INT 
+SET @Counter=1
+WHILE
+BEGIN
+    INSERT INTO Patient(Timestamp, O2Level) Values ('20100301', @Counter)
+    PRINT 'The counter value is = ' + CONVERT(VARCHAR,@Counter)
+    SET @Counter  = @Counter  + 1
+   -- WAITFOR DELAY '00:00:01';
+END
