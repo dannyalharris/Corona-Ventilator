0e15015944f7e1b6998b2ee81e136c617af6b479
Danial Haris <39772417+dannyalharris@users.noreply.github.com>
2021-06-06 02:16:32 +0200

    Update SQL-QUERY-CHART-TEST.sql

--- SQL Query Script/SQL-QUERY-CHART-TEST.sql
+++ SQL Query Script/SQL-QUERY-CHART-TEST.sql
@@ -1,11 +1,10 @@
 ﻿USE [Corona-Ventilator-Database]
 GO
-DELETE FROM Patient
 DECLARE @Counter INT 
 SET @Counter=1
 WHILE (1=1)
 BEGIN
-    INSERT INTO Patient(Timestamp, O2Level) Values ('20100301', @Counter)
+    INSERT INTO Patient(Timestamp, O2Level) Values ('2021-10-10 10:10:21', @Counter)
     PRINT 'The counter value is = ' + CONVERT(VARCHAR,@Counter)
     SET @Counter  = @Counter  + 1
     WAITFOR DELAY '00:00:01';
